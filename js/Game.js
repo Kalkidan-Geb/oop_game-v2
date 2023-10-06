e25bc4456c23171fc550336ad539bc6861a2e022
Kalkidan-Geb <144774340+Kalkidan-Geb@users.noreply.github.com>
2023-10-06 16:42:25 -0700

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -14,58 +14,71 @@ class Game {
         ];
         this.activePhrase = null;
     }
-    startGame(){
-        const startScreen = document.querySelector()
-        startScreenOverlay.style.display= 'show';
-        startScreenOverlay.style.display = 'none';
+    startGame() {
+        const startScreen = document.querySelector('#overlay');
+        startScreen.style.display = 'show';
+        startScreen.style.display = 'none';
 
         this.activePhrase = this.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
+        console.log(this.activePhrase);
     }
-    getRandomPhrase(){
+    getRandomPhrase() {
         const randomPhrase = Math.floor(Math.random() * this.phrases.length)
         return this.phrases[randomPhrase];
     }
     handleInteraction(button) {
-        if(this.activePhrase.phrase.includes(button.innerHTML)) {
+        if (this.activePhrase.phrase.includes(button.innerHTML)) {
             button.classList.add('chosen');
-            button.disbaled = true;
-            this.activePhrase.showMathcedLetter(button.innerHTML);
+            button.disabled = true;
+            this.activePhrase.showMatchedLetter(button.innerHTML);
 
-        if (this.checkForWin() === true) {
-            this.gameOver(this.checkForWin());
+            if (this.checkForWin() === true) {
+                this.gameOver(this.checkForWin());
 
-            }else {
+            } 
+        } else {
                 button.classList.add('wrong');
                 button.disabled = true;
                 this.removeLife();
             }
-        }
-removeLife() 
-    this.missed ++;
+    }
+    removeLife() {
         const remainingLives = document.querySelectorAll('.tries img');
         remainingLives[this.missed].src = 'images/lostHeart.png';
-// if maxiumum amount of guesses have been reached
-    if (this.missed >= 5) {
-        this.gameOver();
-    }
-}}
+        this.missed++;
+        // if maxiumum amount of guesses have been reached
+        if (this.missed >= 5) {
+            this.gameOver(false);
+        }
+    };
 
-checkforWin(); {
-    const hiddenLetters = document.querySelectorAll('.hide');
-    return hiddenLetters.length === 0;
+    checkForWin() {
+        const hiddenLetters = document.querySelectorAll('.hide');
+        return hiddenLetters.length === 0;
+    };
+
+    gameOver(gameWon) {
+        const startScreen = document.querySelector('#overlay');
+        const gameOverMessage = document.querySelector('#game-over-message');
+        startScreen.style.display = 'block';
+        if (gameWon === true) {
+            gameOverMessage.textContent = "Correct!";
+            startScreen.classList.remove('lose');
+            startScreen.classList.add('win');
+        } else {
+            gameOverMessage.textContent = "Not quite! Try again!";
+            startScreen.classList.remove('win');
+            startScreen.classList.add('lose');
+            
+        }
+    }
+    
+       
+
+
+
+        // remove all li elements
+
+       // console.log("reset");
 }
-
-gameOver(gameWon); {
-    startScreen.style.display = 'block';
-    if (gameWon === true) {
-        gameOverMessage.text.Content = "Correct!";
-        startScreen.classList.remove('lose');
-        startScreen.classList.add('win');
-    } else {
-        gameOverMessage.textContent = "Not quite! Try again!";
-        startScreen.classlist.remove('win');
-        startScreen.classList.add('lose');
-    }
-    this.reset();
-    }
